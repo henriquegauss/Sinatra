bb83dd98c86431361229636a1a76481b6a7e665e
jesusCristo <parolajc@hotmail.com>
2021-02-13 11:48:09 +0000

    Adicionei primeiros ficheiros do site

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'sinatra/contrib' # disponibilizar diversas novas funções comuns
+require 'sinatra/reloader'
+
+get '/channels' do
+    'Hello Ruben tudo bem? aaaa'
+end
+
+get '/calls' do
+    'Paz'
+end
